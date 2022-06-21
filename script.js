03928e8b0c2c214dce504f68b21c1292cb40576e
Jigar Patel <85083158+jpatel98@users.noreply.github.com>
2022-06-21 10:34:24 -0400

    Final edit

--- script.js
+++ script.js
@@ -1,132 +1,193 @@
-let dogList = [];
-let dogName = "";
-const dog = {
-  name: "",
-  image: "",
-};
-let options = [];
+const dogsURL = 'https://dog.ceo/api/breeds/list/all';
 
-axios
-  .get("https://dog.ceo/api/breeds/list/all")
-  .then((response) => {
-    dogList = response.data.message;
+let score = 0;
+let rounds = 0;
+let dogs = [];
+let dogOptions = [];
 
-    //Wrong options should not be equal to correct ones.
-    //Options should not be same.
+const dogContainer = document.querySelector('.container__img');
+const playButton = document.createElement('button');
 
-    let correctIndex = randomNumber();
-    wrongOption1 = randomNumber();
-    wrongOption2 = randomNumber();
-    wrongOption3 = randomNumber();
-    if (((correctIndex == wrongOption1) == wrongOption2) == wrongOption3) {
-      correctIndex = randomNumber();
-      wrongOption1 = randomNumber();
-      wrongOption2 = randomNumber();
-      wrongOption3 = randomNumber();
-      console.log("options matched!!!!");
-    } else {
-      // console.log(correctIndex, wrongOption1, wrongOption2, wrongOption3);
-      dogName = generateOptions(dogList, correctIndex);
-      wrongOption1 = generateOptions(dogList, wrongOption1);
-      wrongOption2 = generateOptions(dogList, wrongOption2);
-      wrongOption3 = generateOptions(dogList, wrongOption3);
-      options.push(dogName, wrongOption1, wrongOption2, wrongOption3);
-      console.log(options);
-    }
+playButton.classList.add('container__form-btn');
+playButton.innerText = 'Play Game?'
+dogContainer.appendChild(playButton);
 
-    return dogName;
-  })
-  .then((name) => {
+function playRound (event) {
+    event.preventDefault();
+
+    dogContainer.innerHTML = '';
+    dogOptions = [];
+
     axios
-      .get(`https://dog.ceo/api/breed/${name}/images/random`)
-      .then((response) => {
-        dog.name = name;
-        dog.image = response.data.message;
-        console.log(response);
-        displayDog();
-      });
-  })
-  .catch((err) => {
-    console.log(
-      "Cannot receive the Dog Information at this time, please try again!",
-      err
-    );
-  });
-const card = document.querySelector(".container__card");
-const form = document.querySelector(".container__form-actual");
+    .get(dogsURL)
+    .then((response) => {
+        dogs = Object.keys(response.data.message);
+        const randomDog = dogs[Math.floor(Math.random() * 96)];
+        return randomDog;
+    })
+    .then((randomDog) => {
+        axios 
+            .get(`https://dog.ceo/api/breed/${randomDog}/images/random`)
+            .then((response) => {
 
-form.addEventListener("submit", handleSubmit);
-function handleSubmit(event) {
-  const radio = document.querySelectorAll(".container__form__radio");
-  const title = document.querySelector(".container__title");
-  console.log(radio);
-  event.preventDefault();
-  console.log("submit clicked");
-  for (var i = 0, length = radio.length; i < length; i++) {
-    if (radio[i].checked) {
-      if (dog.name === radio[i].value) {
-        console.log("Correct answer");
-        title.innerHTML = "Correct answer! Well Done";
-      } else {
-        console.log("Wrong ansswer, try agian!");
-        title.innerHTML = "Wrong Answer! Try Again!";
-      }
+                const dogName = randomDog;
+                const dogImage = response.data.message;
 
-      break;
-    }
-  }
+                const dogImageEl = document.createElement('img');
+                dogImageEl.classList.add('container__img-actual');
+                dogImageEl.setAttribute('src', dogImage);
+                dogImageEl.setAttribute('alt', 'a random doggo')
+                dogContainer.appendChild(dogImageEl);
+
+
+                const formContainer = document.querySelector('.container__form');
+
+                formContainer.innerHTML = '';
+                
+                const formEl = document.createElement('form');
+                formEl.classList.add('container__form-actual');
+                formContainer.appendChild(formEl);
+
+
+                //option 1
+                const label1 = document.createElement('label');
+                label1.classList.add('option1');
+                label1.setAttribute('for', 'option1');
+                label1.innerText = dogName;
+
+                const option1 = document.createElement('input');
+                option1.setAttribute('type', 'radio');
+                option1.setAttribute('name', 'answer');
+                option1.setAttribute('value', dogName);
+                label1.appendChild(option1);
+               
+                dogOptions.push(label1);
+
+        
+                //option 2
+                let wrongOption2 = dogs[Math.floor(Math.random() * 96)];
+                if (wrongOption2 === dogName) {
+                    wrongOption2 = dogs[Math.floor(Math.random() * 96)];
+                }
+                const label2 = document.createElement('label');
+                label2.classList.add('option2');
+                label2.setAttribute('for', 'option2');
+                label2.innerText = wrongOption2;
+
+                const option2 = document.createElement('input');
+                option2.setAttribute('type', 'radio');
+                option2.setAttribute('name', 'answer');
+                option2.setAttribute('value', wrongOption2);
+                label2.appendChild(option2);
+
+                dogOptions.push(label2);
+      
+                //option 3
+                let wrongOption3 = dogs[Math.floor(Math.random() * 96)];
+                if (wrongOption3 === dogName || wrongOption3 === wrongOption2) {
+                    wrongOption3 = dogs[Math.floor(Math.random() * 96)];
+                }
+                const label3 = document.createElement('label');
+                label3.classList.add('option3');
+                label3.setAttribute('for', 'option3');
+                label3.innerText = wrongOption3;
+
+                const option3 = document.createElement('input');
+                option3.setAttribute('type', 'radio');
+                option3.setAttribute('name', 'answer');
+                option3.setAttribute('value', wrongOption3);
+                label3.appendChild(option3);
+
+                dogOptions.push(label3);
+
+                //option 4
+                let wrongOption4 = dogs[Math.floor(Math.random() * 96)];
+                if (wrongOption4 === dogName || wrongOption4 === wrongOption3 || wrongOption4 === wrongOption2) {
+                    wrongOption4 = dogs[Math.floor(Math.random() * 96)];
+                }
+                const label4 = document.createElement('label');
+                label4.classList.add('option4');
+                label4.setAttribute('for', 'option4');
+                label4.innerText = wrongOption4;
+
+                const option4 = document.createElement('input');
+                option4.setAttribute('type', 'radio');
+                option4.setAttribute('name', 'answer');
+                option4.setAttribute('value', wrongOption4);
+                label4.appendChild(option4);
+
+                dogOptions.push(label4);
+
+                //shuffle options
+                function shuffle(array) {
+                for (let i = array.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[j]] = [array[j], array[i]];
+                }
+                return array;
+                }
+
+                const shuffledOptions = shuffle(dogOptions);
+                shuffledOptions.forEach((shuffledOption) => {
+                    formEl.appendChild(shuffledOption);
+                });
+                
+
+                //button
+                const submitButton = document.createElement('input');
+                submitButton.setAttribute('type', 'submit');
+                submitButton.setAttribute('value', 'Submit');
+                submitButton.classList.add('container__form-btn');
+                submitButton.innerText = "Submit"
+                formEl.appendChild(submitButton);
+
+                //reset button
+                // const resetButton = document.createElement('input');
+                // resetButton.setAttribute('type', 'reset');
+                // resetButton.classList.add('container__form-btn');
+                // // formContainer.removeChild(output);
+                // formEl.appendChild(resetButton);
+
+                //output status
+                const output = document.createElement('p');
+                output.classList.add('container__card-heading');
+                formContainer.appendChild(output);
+
+
+                //check for correct option
+                const checkAnswer = ((event) => {
+                    event.preventDefault();
+
+                    const selectedAnswer = event.target.answer.value;
+
+                    if (selectedAnswer === dogName) {
+                        score += 1;
+                        output.innerText = `Correct answer. \n The breed is ${dogName}`;
+                        output.style.color = 'green';
+                        formEl.classList.add("hidden");
+                        formContainer.appendChild(nextButton);
+                    } else {
+                        output.innerText = 'Wrong answer. Try again!'
+                        output.style.color = 'red';
+                        score = 0; // we want to change the count back to zero if someone looses.
+                    }
+                });
+
+                formEl.addEventListener('submit', checkAnswer);
+            })   
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+    rounds += 1; 
 }
 
-function displayDog() {
-  //Creating the image element for image
-  const image = document.querySelector(".container__img-actual");
 
-  image.setAttribute("src", dog.image);
+const nextButton = document.createElement('button');
+nextButton.classList.add('container__form-btn');
+nextButton.innerText = 'Next';
 
-  options.forEach((value) => {
-    console.log(value);
-    //div
-    const container = document.createElement("div");
-    container.classList.add("container__form__panel");
-    form.appendChild(container);
 
-    //Radio Button
-    const radioButton = document.createElement("input");
-    radioButton.setAttribute("type", "radio");
-    radioButton.classList.add("container__form__radio");
-    radioButton.value = value;
-    radioButton.id = value;
-    radioButton.name = "radio";
-    container.appendChild(radioButton);
-
-    //Label
-    const label = document.createElement("label");
-    label.setAttribute("for", value);
-    label.classList.add("container__form__label");
-    container.appendChild(label);
-    label.innerHTML = value;
-  });
-
-  //Submit Button
-  const submitButton = document.createElement("button");
-  submitButton.classList.add("container__form-btn");
-  submitButton.setAttribute("type", "submit");
-  submitButton.innerHTML = "Submit";
-  form.appendChild(submitButton);
-
-  // Refresh Button
-  const refreshButton = document.createElement("button");
-  refreshButton.classList.add("container__form-btn");
-  refreshButton.innerHTML = "Refresh";
-  form.appendChild(refreshButton);
-  console.log();
-}
-
-function randomNumber() {
-  const index = Math.floor(Math.random() * 96);
-  return index;
-}
-function generateOptions(list, index) {
-  let name = Object.keys(list)[index];
-  return name;
-}
+nextButton.addEventListener('click', playRound);
+playButton.addEventListener('click', playRound);
